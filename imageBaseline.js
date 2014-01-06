9c9dd07a2c48bebcc3607acdfc67a8cc38521344
tonypine <tony@tonypine.com>
2014-01-06 15:04:25 -0200

    Add crop and check if image are not loaded.
    - Crop
    - isLoaded method

--- imageBaseline.js
+++ imageBaseline.js
@@ -1,22 +1,47 @@
 (function($){
-    
+
     var o = {
         baseline: 11,
-        maxWidth: false
-    };
+        maxWidth: false,
+        parent: {},
+        heightOffset: 0,
+        cropper: {}
+    },
     
-    $.fn.imageBaseline = function( options ) {
-        
-        var settings = $.extend({}, o, options);
-        
-        this.each(function() {
+    m = {
+        init: function( options ) {
             
-            var $this = $(this);
-            $this.data(settings);
-            var op = $this.data();
+            var settings = $.extend({}, o, options);
+    
+            this.each(function() {
+                var $this = $(this);
+                $this.data(settings);
+                var op = $this.data();
+                
+                if ( m.isLoaded.apply( this ) )
+                    m.adjust.apply( this );
+                else
+                    $this.load( $.proxy( m.adjust, this ) );
+    
+            });
+            
+        },
+        isLoaded: function( cb ) {
+            
+            var $this = $(this);            
+            if(this.complete && this.width !== 0)
+                return true;
+            else 
+                return false;
+            
+        },
+        adjust: function() {
+            
+            var $this = $(this),
+                op = $this.data();
             
             if(op.maxWidth) {
-                parent = $this.parent(),
+                var parent = $.isEmptyObject( op.parent ) ? $this.parent() : $this.closest(op.parent),
                     parentWidth = parent.width();
                 $this.width( parentWidth );
             }
@@ -24,11 +49,20 @@
             var h = $this.height(),
                 newH = Math.floor( h / op.baseline ) * op.baseline;
             
-            $this.height( newH );
-            $this.width('auto');
+            if( $.isEmptyObject( op.cropper ) ) {
+                $this.height( newH + op.heightOffset );
+                $this.width('auto');
+            } else {
+                $this.closest(op.cropper).height( newH + op.heightOffset ).css({
+                    overflow: 'hidden'
+                });
+            }
             
-        });
-        
+        }
+    };
+
+    $.fn.imageBaseline = function( method ) {
+        m.init.apply( this, arguments );
     }
-    
+
 })(jQuery);
